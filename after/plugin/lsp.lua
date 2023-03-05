f396560e455b2650077ba874ed210bc47a005d67
Rick de Boer <rick.deboer@live.nl>
2023-03-05 15:08:31 +0100

    feat: nvim-ts-autotag

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,7 @@
+local nvim_lsp = require("lspconfig")
+
+nvim_lsp.tsserver.setup {
+  on_attach = on_attach,
+  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+  cmd = { "typescript-language-server", "--stdio" }
+}
