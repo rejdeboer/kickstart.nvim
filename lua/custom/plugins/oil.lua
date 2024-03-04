eef9a9c0fa7c7d80e812423e251faee0f9b88e56
rejdeboer <rick.deboer@live.nl>
2024-03-04 19:48:09 +0100

    feat: oil

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,17 @@
+return {
+  'stevearc/oil.nvim',
+  opts = {},
+
+  -- Optional dependencies
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+  config = function()
+    require('oil').setup {
+      default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      },
+    }
+    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+  end,
+}
