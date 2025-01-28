93a37686a040bba9a5d442d49d635a4b430a204a
rejdeboer <rick.deboer@live.nl>
2025-01-28 20:35:41 +0100

    feat: disable clangd header insertion

--- init.lua
+++ init.lua
@@ -453,6 +453,8 @@ require('lazy').setup {
             'clangd',
             '--background-index',
             '--clang-tidy',
+            -- clangd keeps importing headers that have already been imported
+            '--header-insertion=never',
             --'--log=verbose',
           },
         },
