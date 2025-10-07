83e3eff996e2e36e2c32b4327054af9575c09db8
rejdeboer <rick.deboer@live.nl>
2025-10-07 13:48:54 +0200

    feat: comment out redundant godot lsp code

--- lua/custom/configs/lsp.lua
+++ lua/custom/configs/lsp.lua
@@ -22,15 +22,15 @@ vim.diagnostic.config {
 
 -- vim.lsp.set_log_level 'trace'
 
-local godot_port = os.getenv 'GDScript_Port' or 6005
-local cmd = vim.lsp.rpc.connect('127.0.0.1', godot_port)
-local pipe = '/tmp/godot.pipe'
-
-vim.lsp.start {
-  name = 'Godot',
-  cmd = cmd,
-  root_dir = vim.fs.dirname(vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]),
-  on_attach = function(client, bufnr)
-    vim.api.nvim_command('echo serverstart("' .. pipe .. '")')
-  end,
-}
+-- local godot_port = os.getenv 'GDScript_Port' or 6005
+-- local cmd = vim.lsp.rpc.connect('127.0.0.1', godot_port)
+-- local pipe = '/tmp/godot.pipe'
+--
+-- vim.lsp.start {
+--   name = 'Godot',
+--   cmd = cmd,
+--   root_dir = vim.fs.dirname(vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]),
+--   on_attach = function(client, bufnr)
+--     vim.api.nvim_command('echo serverstart("' .. pipe .. '")')
+--   end,
+-- }
